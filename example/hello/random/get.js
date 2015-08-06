7ecc9e6a2f7cae38cefa34df139cbaf8223e704d
Luca Bernardo Ciddio <bcluca@gmail.com>
2015-08-06 13:52:27 -0700

    Added support for JavaScript stubs

---
+++ example/hello/random/get.js
@@ -0,0 +1,8 @@
+module.exports = {
+    foo : {
+        bar    : 42,
+        random : function () {
+            return Math.random();
+        }
+    }
+};
--- index.js
+++ index.js
@@ -14,21 +14,44 @@ function factory() {
     return new Backstubber();
 }
 
+function transform(stub) {
+    var obj = {};
+    Object.keys(stub).forEach(function (attr) {
+        var value = stub[attr];
+        switch (typeof value) {
+            case 'object':
+                obj[attr] = transform(value);
+                break;
+            case 'function':
+                obj[attr] = value();
+                break;
+            default:
+                obj[attr] = value;
+        }
+    });
+    return obj;
+}
+
 Backstubber.prototype.mount = function (dir, fallback) {
     var self = this;
-    glob.sync(dir + '/**/*.json').forEach(function (filePath) {
-        var relativePath = path.relative(dir, filePath);
-        var endpoint = path.dirname(relativePath);
-        var verb = path.basename(filePath, '.json');
-        var file = require(filePath);
+    glob.sync(dir + '/**/*.@(json|js)').forEach(function (filePath) {
+        var extension = path.extname(filePath);
+        var verb = path.basename(filePath, extension);
 
         if (VERBS.indexOf(verb) === -1) {
             throw new Error("unknown verb '" + verb + "'");
         }
 
+        var relativePath = path.relative(dir, filePath);
+        var endpoint = path.dirname(relativePath);
+
         self.app[verb]('/' + endpoint, function (req, res) {
+            var stub = require(filePath);
+            if (extension === '.js') {
+                stub = transform(stub);
+            }
             res.setHeader('Content-Type', 'application/json');
-            res.send(file);
+            res.send(stub);
         });
     });
     return this;
