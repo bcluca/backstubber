5c368c9b8c953280d4d9c6ff1c4d28e3a4748228
Luca Bernardo Ciddio <bcluca@gmail.com>
2015-08-06 11:49:39 -0700

    Serving JSON from mounted dir

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 var pkg = require('../package.json');
+var backstubber = require('..');
 
 console.log(pkg.name + ' ' + pkg.version);
+
+backstubber().mount(__dirname + '/../example').listen(3333);
--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 'use strict';
 
-module.exports = {};
+var express = require('express');
+var glob = require('glob');
+var path = require('path');
+
+var VERBS = ['head', 'get', 'post', 'put', 'delete', 'all'];
+
+function Backstubber() {
+    this.app = express();
+}
+
+function factory() {
+    return new Backstubber();
+}
+
+Backstubber.prototype.mount = function (dir, fallback) {
+    var self = this;
+    glob.sync(dir + '/**/*.json').forEach(function (filePath) {
+        var relativePath = path.relative(dir, filePath);
+        var endpoint = path.dirname(relativePath);
+        var verb = path.basename(filePath, '.json');
+        var file = require(filePath);
+
+        if (VERBS.indexOf(verb) === -1) {
+            throw new Error("unknown verb '" + verb + "'");
+        }
+
+        self.app[verb]('/' + endpoint, function (req, res) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(file);
+        });
+    });
+    return this;
+};
+
+Backstubber.prototype.listen = function () {
+    this.app.listen.apply(this.app, arguments);
+    return this;
+};
+
+module.exports = factory;
