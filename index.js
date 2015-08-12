52dd60172fd146e32fb35beeef9b17a919084dbe
Luca Bernardo Ciddio <bcluca@gmail.com>
2015-08-12 01:29:26 -0700

    Updated version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
 * Simple yet powerful backend stubs.
 * @module backstubber
-* @version v0.1.11
+* @version v0.1.12
 * @license MIT
 * @author {@link https://github.com/bcluca|Luca Bernardo Ciddio}
 */
